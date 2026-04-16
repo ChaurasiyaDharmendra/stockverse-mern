ebe65c25bf7d1a155bc45d91e720a401d197417b
ChaurasiyaDharmendra <golu78dk@gmail.com>
2026-04-17 01:49:52 +0530

    dashboard topbar color fix

--- dashboard/src/components/TopBar.js
+++ dashboard/src/components/TopBar.js
@@ -1,10 +1,37 @@
+// import React from "react";
+
+// import Menu from "./Menu";
+
+// const TopBar = () => {
+//   return (
+//        <div className="topbar-container dark-topbar">
+//       <div className="indices-container">
+//         <div className="nifty">
+//           <p className="index">NIFTY 50</p>
+//           <p className="index-points">{100.2} </p>
+//           <p className="percent"> </p>
+//         </div>
+//         <div className="sensex">
+//           <p className="index">SENSEX</p>
+//           <p className="index-points">{100.2}</p>
+//           <p className="percent"></p>
+//         </div>
+//       </div>
+
+//       <Menu />
+//     </div>
+//   );
+// };
+
+// export default TopBar;
+
 import React from "react";
 
 import Menu from "./Menu";
 
 const TopBar = () => {
   return (
-    <div className="topbar-container">
+       <div className="topbar-container dark-topbar">
       <div className="indices-container">
         <div className="nifty">
           <p className="index">NIFTY 50</p>
@@ -24,4 +51,3 @@ const TopBar = () => {
 };
 
 export default TopBar;
-
