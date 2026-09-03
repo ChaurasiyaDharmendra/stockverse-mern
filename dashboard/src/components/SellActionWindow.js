283aa1915290cb5207dc36abeb2675157a062853
ChaurasiyaDharmendra <golu78dk@gmail.com>
2026-09-04 00:03:02 +0530

    fix sell order

--- dashboard/src/components/SellActionWindow.js
+++ dashboard/src/components/SellActionWindow.js
@@ -1,70 +1,90 @@
-import React,{useContext, useState} from 'react';
-import axios from 'axios';
-import GeneralContext from './GeneralContext';
-import './BuyActionWindow.css';
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import GeneralContext from "./GeneralContext";
+import "./BuyActionWindow.css";
 import { Link } from "react-router-dom";
 
+const SellActionWindow = ({ uid }) => {
+  const [stockQuantity, setStockQuantity] = useState(1);
+  const [stockPrice, setStockPrice] = useState(0.0);
 
-const SellActionWindow = ({uid})=>{
-    const [stockQuantity,setStockQuantity] =useState(1);
-    const [stockPrice , setStockPrice] = useState(0.0);
-    const {closeSellWindow} = useContext(GeneralContext);
+  const { closeSellWindow } = useContext(GeneralContext);
 
-    const handleSellClick = () =>{
-        axios.post("https://zerodhaclone-bd.onrender.com/newOrder",{
-            name:uid,
-            qty:stockQuantity,
-            price:stockPrice,
-            mode:"SELL"
-        });
-        closeSellWindow();
+  const handleSellClick = async () => {
+    try {
+      await axios.post(
+        "https://stockverse-mern.onrender.com/newOrder",
+        {
+          name: uid,
+          qty: parseFloat(stockQuantity),
+          price: parseFloat(stockPrice),
+          mode: "SELL",
+        }
+      );
+
+      closeSellWindow();
+    } catch (err) {
+      console.error("Sell failed", err);
+      alert("Failed to place sell order. Please try again.");
     }
+  };
 
-    const handleCancelClick = () =>{
-        closeSellWindow();
-    }
+  const handleCancelClick = () => {
+    closeSellWindow();
+  };
 
+  return (
+    <div className="container" id="buy-window" draggable="true">
+      <div className="regular-order">
+        <div className="inputs">
+          <fieldset>
+            <legend>Qty</legend>
 
+            <input
+              type="number"
+              name="qty"
+              id="qty"
+              onChange={(e) => setStockQuantity(e.target.value)}
+              value={stockQuantity}
+            />
+          </fieldset>
 
-    return(
-        <div className="container" id="buy-window" draggable="true">
-            <div className="regular-order">
-                <div className="inputs">
-                    <fieldset>
-                        <legend>Qty</legend>
-                        <input type='number'
-                        name='qty'
-                        id='qty'
-                        onChange={(e)=>{setStockQuantity(e.target.value)}}
-                        value={stockQuantity}
-                        />
-                    </fieldset>
-                    <fieldset>
-                        <legend>price</legend>
-                        <input type='number'
-                        name='price'
-                        id='price'
-                        onChange={(e)=>{setStockPrice(e.target.value)}}
-                        value={stockPrice}
-                        />
-                    </fieldset>
-                </div>
-            </div>
+          <fieldset>
+            <legend>price</legend>
 
-            <div  className="buttons">
-                <span>Margin required ₹140.65</span>
-                <div>
-                     <Link className="btn btn-danger" onClick={handleSellClick}>
-                                sell
-                     </Link>
-                    <Link to="" className="btn btn-grey" onClick={handleCancelClick}>
-                                Cancel
-                       </Link>
-                </div>
-            </div>
+            <input
+              type="number"
+              name="price"
+              id="price"
+              onChange={(e) => setStockPrice(e.target.value)}
+              value={stockPrice}
+            />
+          </fieldset>
         </div>
-    )
+      </div>
 
-}
+      <div className="buttons">
+        <span>Margin required ₹140.65</span>
+
+        <div>
+          <Link
+            className="btn btn-danger"
+            onClick={handleSellClick}
+          >
+            Sell
+          </Link>
+
+          <Link
+            to=""
+            className="btn btn-grey"
+            onClick={handleCancelClick}
+          >
+            Cancel
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SellActionWindow;
