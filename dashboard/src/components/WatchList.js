be507d43304ba1c4383e160d3d263698ec5a4962
ChaurasiyaDharmendra <golu78dk@gmail.com>
2026-09-05 19:50:46 +0530

    Remove search box from watchlist

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -20,10 +20,13 @@ const labels = watchlist.map((subArray) => subArray["name"]);
 const WatchList = () => {
   const data = {
     labels,
+
     datasets: [
       {
         label: "Price",
+
         data: watchlist.map((stock) => stock.price),
+
         backgroundColor: [
           "rgba(255, 99, 132, 0.5)",
           "rgba(54, 162, 235, 0.5)",
@@ -32,6 +35,7 @@ const WatchList = () => {
           "rgba(153, 102, 255, 0.5)",
           "rgba(255, 159, 64, 0.5)",
         ],
+
         borderColor: [
           "rgba(255, 99, 132, 1)",
           "rgba(54, 162, 235, 1)",
@@ -40,6 +44,7 @@ const WatchList = () => {
           "rgba(153, 102, 255, 1)",
           "rgba(255, 159, 64, 1)",
         ],
+
         borderWidth: 1,
       },
     ],
@@ -47,19 +52,6 @@ const WatchList = () => {
 
   return (
     <div className="watchlist-container">
-      <div className="search-container">
-        <input
-          type="text"
-          name="search"
-          id="search"
-          placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
-          className="search"
-        />
-
-        <span className="counts">
-          {watchlist.length} / 50
-        </span>
-      </div>
 
       <ul className="list">
         {watchlist.map((stock, index) => {
@@ -68,6 +60,7 @@ const WatchList = () => {
       </ul>
 
       <DoughnutChart data={data} />
+
     </div>
   );
 };
@@ -77,11 +70,11 @@ export default WatchList;
 const WatchListItem = ({ stock }) => {
   const [showWatchlistActions, setShowWatchlistActions] = useState(false);
 
-  const handleMouseEnter = (e) => {
+  const handleMouseEnter = () => {
     setShowWatchlistActions(true);
   };
 
-  const handleMouseLeave = (e) => {
+  const handleMouseLeave = () => {
     setShowWatchlistActions(false);
   };
 
@@ -91,11 +84,13 @@ const WatchListItem = ({ stock }) => {
       onMouseLeave={handleMouseLeave}
     >
       <div className="item">
+
         <p className={stock.isDown ? "down" : "up"}>
           {stock.name}
         </p>
 
         <div className="itemInfo">
+
           <span className="percent">
             {stock.percent}
           </span>
@@ -109,12 +104,14 @@ const WatchListItem = ({ stock }) => {
           <span className="price">
             {stock.price}
           </span>
+
         </div>
       </div>
 
       {showWatchlistActions && (
         <WatchListActions uid={stock.name} />
       )}
+
     </li>
   );
 };
@@ -132,7 +129,9 @@ const WatchListActions = ({ uid }) => {
 
   return (
     <span className="actions">
+
       <span>
+
         <Tooltip
           title="Buy (B)"
           placement="top"
@@ -182,7 +181,9 @@ const WatchListActions = ({ uid }) => {
             <MoreHoriz className="icon" />
           </button>
         </Tooltip>
+
       </span>
+
     </span>
   );
 };
